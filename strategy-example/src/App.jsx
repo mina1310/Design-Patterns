2d15e8cc0be52e463bcc42e2fa0154904ca2fa46
mina1310 <mina.kavianig@gmail.com>
2026-02-08 23:41:00 +0330

    feat:create some changes in stategy pattern

--- strategy-example/src/App.jsx
+++ strategy-example/src/App.jsx
@@ -1,28 +1,35 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import { creditPay } from "./strategy/creditPay";
 import { cryptoPay } from "./strategy/cryptoPay";
 
+const strategies = {
+  credit: creditPay,
+  crypto: cryptoPay,
+};
+
 function App() {
-  const [pay, setPay] = useState(creditPay);
+  const [pay, setPay] = useState(() => strategies.credit);
+
   const handleChange = (e) => {
-    const value = e.target.value;
-    if (value === "credit") setPay(() => creditPay);
-    if (value === "crypto") setPay(() => cryptoPay);
+    setPay(() => strategies[e.target.value]);
   };
+
   const handleClick = () => {
     pay(100);
   };
+
   return (
     <>
       <h3>شما میتوانید روش پرداخت خود را انتخاب نمایید</h3>
+
       <select onChange={handleChange}>
         <option value="credit">کارت اعتباری</option>
         <option value="crypto">ارز دیجیتال</option>
       </select>
-      <button onClick={handleClick}> پرداخت </button>
+
+      <button onClick={handleClick}>پرداخت</button>
     </>
   );
 }
